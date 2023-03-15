58e24e38cac19395d9b0000e91963480cdb9c192
Elthon <elthonferreira18@gmail.com>
2023-03-15 12:21:38 -0300

    Criando diretivas parte 2 (foco em máscaras do tipo Date)

---
+++ Aula 11 - Criando Diretivas parte 2/js/directives/uiAlertDirective.js
@@ -0,0 +1,14 @@
+angular.module("listaTelefonica").directive("uiAlert", function () {
+
+	return {
+		templateUrl: "view/alert.html",
+		replace: true,
+		restrict: "AE",
+		scope: {
+			title: "@",
+			message: "="
+		},
+		transclude: true
+	};
+
+});
